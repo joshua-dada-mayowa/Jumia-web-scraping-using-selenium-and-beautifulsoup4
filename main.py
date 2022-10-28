57fd966c43094eaf6146e2f7e8b7b00b45d336f4
Joshua Dada <96214236+joshua-dada-mayowa@users.noreply.github.com>
2022-10-28 15:35:40 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+
+PATH="C:\Program Files (x86)\chromedriver.exe"
+driver= webdriver.Chrome(PATH)
+
+url="https://www.jumia.com.ng/flash-sales/"
+driver.get(url)
+print(driver.title)
+search= driver.find_element(By.ID,'fi-q')
+
+search.send_keys("Nike")
+search.send_keys(Keys.RETURN)
+
+time.sleep(5)
